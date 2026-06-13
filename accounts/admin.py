999873ee0616435fba94f626b96a2d8f4f7b4d97
Yash Tiwari <YOUR_GITHUB_EMAIL>
2026-06-13 18:12:51 +0530

    Register vehicle admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Enquiry
-
-# Register your models here.
-from .models import Enquiry
+from .models import Enquiry, Vehicle
 
 @admin.register(Enquiry)
 class EnquiryAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'phone', 'vehicle', 'created_at']
     search_fields = ['name', 'email', 'vehicle']
     list_filter = ['created_at', 'vehicle']
+
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'price']
+    search_fields = ['name', 'brand']
